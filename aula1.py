bcafccf0badaf98786f7cfcd20a42f508412c1be
Alekkafka999 <alesankafka@gmail.com>
2025-11-16 08:47:19 -0300

    Curso de Python - UDEMY
    Seção 3 - Vídeo 17

---
+++ aula1.py
@@ -0,0 +1,15 @@
+# Permite escrever um comentário
+print("Olá, Mundo!")  # Exibe uma mensagem na tela
+print("Bem-vindo à aula de Python.")
+"""
+DOC STRING
+Deixe um comentário malcriado entre as aspas triplas
+"""
+
+'''
+doc string
+pode ser entre tripla aspas simples
+DEIXE MAIS UM COMENTÁRIO MALCRIADO ENTRE AS ASPAS SIMPLES TRIPLAS
+
+'''
+
