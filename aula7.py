815595b532b75015fe244a4e4b692debd21f8b18
Alekkafka999 <alesankafka@gmail.com>
2025-11-18 08:47:46 -0300

    Vídeos 22 até 25
    Udemy Academy

---
+++ aula7.py
@@ -0,0 +1,18 @@
+'''
+Variáveis são usadas para salvar algo na memória do computador.
+PEP8: inicie variáveis com letras minúsculas, pode usar números e underline _.
+O sinal = é o operador da atribuição. Ele é usado para atribuir um valor a uma variável (nome).
+Uso: nome_variavel = expressão
+
+'''
+nome = 'Luiz'
+idade = 17
+maior_de_idade = idade >= 18
+
+print('Nome:', nome)
+print('Idade:', idade)
+print('Maior de idade:', maior_de_idade)
+
+
+
+
---
+++ aula8.py
@@ -0,0 +1,17 @@
+'''
+Exercícios
+
+'''
+nome = 'Alessandra'
+sobrenome = 'Kafka'
+idade = 50
+ano_nascimento = 2025 - idade
+maior_de_idade = idade >= 18
+altura_metros = 1.60
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
