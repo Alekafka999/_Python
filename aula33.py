4b60802d24ab367f2483287a08ba85e7cd548bf4
Alekkafka999 <alesankafka@gmail.com>
2025-11-24 08:34:13 -0300

    Vídeos 58 e 59
    58 - Documentação do Python
    59 - Introdução ao Loop Infinito: While

---
+++ aula33.py
@@ -0,0 +1,14 @@
+'''
+https://docs.python.org/pt-br/3/library/stdtypes.html
+
+Imutáveis vistos até o vídeo 58: str, int, float, bool
+
+'''
+
+string = 'luiz otávio'
+outra_variavel = f'{string[:3]}ABC{string[4:]}'
+
+print(string)
+print(outra_variavel)
+print(string.capitalize())
+print(string.zfill(28))
