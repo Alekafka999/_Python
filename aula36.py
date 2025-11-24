acac52df0c24f1b5f9c475258a54dd60464a7a67
Alekkafka999 <alesankafka@gmail.com>
2025-11-24 09:33:45 -0300

    Video 61
    Operadores de Atribuição

---
+++ aula36.py
@@ -0,0 +1,19 @@
+'''
+
+Operadores de atribuição
+= += -= *= /= //= **= %=
+
+
+contador = 0
+
+while contador <=7:
+        contador = contador + 1
+        print(contador)
+
+print('Acabou')
+'''
+
+
+contador = 10
+contador %= 
+print(contador)
