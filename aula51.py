94ffc56b2029ff9db02029df77a59b9caedc396a
Alekkafka999 <alesankafka@gmail.com>
2025-12-11 16:53:03 -0300

    Update aula51.py

--- aula51.py
+++ aula51.py
@@ -1,12 +1,16 @@
 '''
 Docstring for aula51
-Introdução ao desempacotamento
+Introdução ao desempacotamento + tuples (tuplas)
 
 _, _, nome, *resto = ['Maria', 'Helena', 'Luiz']
 
 print(nome)
 
-'''
 _, nome, *resto = ['Maria', 'Helena', 'Luiz']
 nome1, *_ = ['Maria', 'Helena', 'Luiz']
 print(nome1, _)
+
+'''
+
+nomes = ['Maria', 'Helena', 'Luiz']
+nome1, nome2, nome3 = nomes
